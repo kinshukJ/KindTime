22d586e205d12f03d4e11556b77ea9a682aa4b5d
Kinshuk <kinshukjuneja@gmail.com>
2023-12-28 14:47:49 -0700

    Initial Commit

---
+++ KindTime/KindTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KindTimeApp.swift
+//  KindTime
+//
+//  Created by Kinshuk Juneja on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct KindTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
